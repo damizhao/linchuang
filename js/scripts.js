6330163acbd55633b82c51e65a6201448620bb41
zhaorong <305665098@qq.com>
2016-04-26 15:56:49 +0800

    linchuang

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var designCom = document.getElementById('designCom');
+var product = document.getElementById('product');
+var designPer  = document.getElementById('designPer');
+var person  = document.getElementById('person');
+designCom.addEventListener("touchend", function(){
+	document.location.href='index.html';
+});
+designCom.addEventListener("click", function(){
+	document.location.href='index.html';
+});
+product.addEventListener("touchend", function(){
+	document.location.href='product.html';
+});
+product.addEventListener("click", function(){
+	document.location.href='product.html';
+});
+designPer.addEventListener("touchend", function(){
+	document.location.href='design.html';
+});
+designPer.addEventListener("click", function(){
+	document.location.href='design.html';
+});
+person.addEventListener("touchend", function(){
+	document.location.href='person.html';
+});
+person.addEventListener("click", function(){
+	document.location.href='person.html';
+});
